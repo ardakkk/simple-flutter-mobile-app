dc64c3139f02207159f1a0c775e1fdf4ef7470d9
ardakkk <ardakfuse@gmail.com>
2019-05-22 10:33:34 +0600

    Removed last 2 Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -169,16 +169,6 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 50, 
               color: Colors.amber[600],
               child:  const Center(child: Text('Entry A'),),
-            ),
-            Container(
-              height: 50, 
-              color: Colors.amber[500],
-              child:  const Center(child: Text('Entry B'),),
-            ),
-            Container(
-              height: 50, 
-              color: Colors.amber[100],
-              child:  const Center(child: Text('Entry C'),),
             )
           ],
         )
